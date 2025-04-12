ef2fbedf22e3090746c03f422da441c32f9dc07d
TarasovFedor <fedor.tarasov.2007@mail.ru>
2025-04-12 12:03:06 +0300

    second commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <img src="./Mospolytech.jpg" alt="Mospolytech logo">
+            <h1>2.1 Домашняя работа: Hello, World!</h1>
+        </header>
+
+        <main>
+            <p> <?php echo "Hello, World!"; ?> </p>
+            </main>
+
+        <footer>задание для самостоятельной работы</footer>
+
+    </div>
+</body>
+</html>
